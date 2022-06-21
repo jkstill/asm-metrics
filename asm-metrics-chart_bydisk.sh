4b4a8badb7ca87a757a728bbb5580b2bde5605f8
Jared Still <jkstill+github@gmail.com>
2022-06-21 16:34:10 -0700

    genericized names

--- asm-metrics-chart_bydisk.sh
+++ asm-metrics-chart_bydisk.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+mkdir -p xlsx-db-bydisk
+
 #./asm-metrics-chart.pl  --debug \
 ./asm-metrics-chart.pl \
 	--worksheet-col DISK_NAME \
-	--spreadsheet-file oravm-asm-metrics-by-disk.xlsx \
+	--spreadsheet-file xlsx-db-bydisk/asm-metrics-by-disk.xlsx \
 	--chart-cols READS AVG_READ_TIME WRITES AVG_WRITE_TIME  \
-	-- logs/asm-oravm-agg-bydisk.csv
+	-- db-bydisk/asm-agg-bydisk.csv
 
